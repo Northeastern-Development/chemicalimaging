edb9955a7bb5257ed1369015354d9c9047bef465
Sean Tierney <s.tierney@northeastern.edu>
2018-10-25 13:30:46 -0400

    updated styles and template for the homepage

---
+++ themes/northeastern/templates/template-homepage.php
@@ -0,0 +1,73 @@
+<?php /* Template Name: Homepage */ get_header(); ?>
+
+	<main role="main">
+
+		<section class="fullwidth hero">
+
+			<!-- <div><img src="/wp-content/uploads/headerimage-120x56.jpg" /></div> -->
+
+			<div><img srcset="/wp-content/uploads/headerimage.jpg 4096w,/wp-content/uploads/headerimage-768x360.jpg 1280w,/wp-content/uploads/headerimage-700x328.jpg 960w" src="/wp-content/uploads/headerimage.jpg" /></div>
+
+			<h1>This is the homepage.  This is the homepage.  This is the homepage.  This is the homepage.  This is the homepage.  This is the homepage.</h1>
+
+		</section>
+
+		<section class="intro">
+			<div>Et harum quidem rerum facilis est et expedita distinctio.</div>
+			<div>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</div>
+		</section>
+
+		<section class="fullwidth news">
+			<h3>Section Title</h3>
+			<ul>
+				<li>
+					<a href="https://www.northeastern.edu" title="Click to read more" target="_blank">
+						<div><img src="/wp-content/uploads/newsimage.jpg" /></div>
+						<p class="title">This is the title of the news article.</p>
+						<p class="author">by Author Name</p>
+						<p>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+						<span>Read More</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.northeastern.edu" title="Click to read more" target="_blank">
+						<div><img src="/wp-content/uploads/newsimage.jpg" /></div>
+						<p class="title">This is the title of the news article.</p>
+						<p class="author">by Author Name</p>
+						<p>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+						<span>Read More</span>
+					</a>
+				</li>
+				<li>
+					<a href="https://www.northeastern.edu" title="Click to read more" target="_blank">
+						<div><img src="/wp-content/uploads/newsimage.jpg" /></div>
+						<p class="title">This is the title of the news article.</p>
+						<p class="author">by Author Name</p>
+						<p>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+						<span>Read More</span>
+					</a>
+				</li>
+			</ul>
+		</section>
+
+		<section class="fullwidth stats">
+			<h3>Section Title</h3>
+			<ul>
+				<li>
+					111%
+					<p>This is the supporting text below the stat</p>
+				</li>
+				<li>
+					111%
+					<p>This is the supporting text below the stat</p>
+				</li>
+				<li>
+					111%
+					<p>This is the supporting text below the stat</p>
+				</li>
+			</ul>
+		</section>
+
+	</main>
+
+<?php get_footer(); ?>
